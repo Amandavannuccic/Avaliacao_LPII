cc9f78c0655a184d9bde11cb95fe639e4e93be07
Amanda Vannucci <127263243+Amandavannuccic@users.noreply.github.com>
2024-10-08 22:23:19 -0300

    strart: Spring
    Project – Escolha "Maven";
    Spring Boot – Mantenha a versão pré-selecionada;
    • Java – Escolha "17" para utilizar o ambiente de desenvolvimento Gitpod;
    • Group – Aqui você coloca algo único de sua organização, para tornar o identificador de seu 
    projeto úinico. Exemplo: "br.gov.sp.bd";
    • Artifact – O nome do seu projeto. Exemplo: "demoprojeto-spring2042";
    • Description – Uma descrição para seu projeto. Exemplo: "Avaliacao Lab BD";
    • Dependencies (clique em Add Dependencies...) – Escolha Spring Boot DevTools, Spring Data 
    JPA e Spring Web

---
+++ src/main/java/br/gov/sp/bd/demoprojeto_spring2042/DemoprojetoSpring2042Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.bd.demoprojeto_spring2042;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojetoSpring2042Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojetoSpring2042Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/bd/demoprojeto_spring2042/DemoprojetoSpring2042ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.bd.demoprojeto_spring2042;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojetoSpring2042ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
